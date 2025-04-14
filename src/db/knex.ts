8e485e740922533e0026867888f633d620781439
shellob <smakgames.studio@gmail.com>
2025-04-14 14:14:58 +0300

    fix docker

--- src/db/knex.ts
+++ src/db/knex.ts
@@ -1,6 +1,5 @@
-import knex from 'knex';
-import config from '../../knexfile';
+import { knex } from 'knex';
+import config from '../config/knexfile';
 
 const db = knex(config);
-
 export default db;
